89c9413ecaca142dcf763c24caa722a44e24b4bf
hugeBlack <wwkq03@outlook.com>
2022-02-13 22:31:22 +0800

    bug fix

--- Program.cs
+++ Program.cs
@@ -11,18 +11,15 @@ using Terraria.Localization;
 using Terraria.UI;
 
 namespace terrariaPlayerEditor
-{//ReLogic.Content.AssetRepository.FindSourceForAsset(string)
+{
     class Program
     {
         static void Main(string[] args)
         {
             Console.Title = "Terraria Player Editor";
-            //string filePath = "C:\\Users\\wkq\\Documents\\My Games\\Terraria\\Players\\tttt.plr";//Console.ReadLine();
             Main game = new Main();
             Utiles.loadResources();
             Console.WriteLine("Load complete. Press any key to continue.");
-
-
             Console.ReadKey();
             ScreenManager sm = new ScreenManager();
             SelectPlayerScreen selectPlayerScreen = new SelectPlayerScreen(sm);
